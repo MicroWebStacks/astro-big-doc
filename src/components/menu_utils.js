7c4dd601cf4f44874b4a68857b8881747206bde7
wass <wassim.filali@gmail.com>
2022-10-15 13:08:40 +0200

    added ToC working refactoring left and right menus

--- src/components/menu_utils.js
+++ src/components/menu_utils.js
@@ -60,24 +60,28 @@ function process_menu_list(url,raw_menu){
     const active_subpage_index = active_subpage(url,side_menu.items)
     side_menu.items.forEach((item,index)=>{
         item.classes = (index == active_subpage_index)?"active":""
-        item.paddingLeft = item.level?item.level*10+10:10
+        item.paddingLeft = item.depth?item.depth*10+10:10
     })
     
 
     return side_menu
 }
 
-function set_classes(url,items){
+function set_classes_recursive(url,items){
     items.forEach((item)=>{
         item.active = (url_path(url) == item.href)
         if("items" in item){
             item.parent = true
             item.expanded = true
-            set_classes(url,item.items)
+            set_classes_recursive(url,item.items)
         }
     })
 }
 
+/**  Topmenu  : depth 0
+ *  Sidemenu : depths 1,2,...
+ *  Sidemenu items : parent,(children)expanded
+ */
 function process_menu_tree(url,raw_menu){
     let side_menu = {items:[],visible:false}
 
@@ -88,15 +92,66 @@ function process_menu_tree(url,raw_menu){
     }
     side_menu.items = raw_menu[active_section_index].items
 
-    set_classes(url,side_menu.items)
+    set_classes_recursive(url,side_menu.items)
 
     return side_menu
 }
 
-function process_toc_list(headings){
-    let toc_menu = {items:[],visible:false}
+function find_parent(index,headings){
+    const element_depth = headings[index].depth
+    if(index == 0){
+        return null
+    }else{
+        for(let rev_i = index-1;rev_i>=0;rev_i--){
+            if(headings[rev_i].depth<element_depth){
+                return headings[rev_i]
+            }
+        }
+    }
+}
 
-    return toc_menu
+function heading_list_to_tree(headings){
+    for(let element of headings){
+        element.items=[]
+        element.parent=true
+        element.expanded=true
+        element.href = `#${element.slug}`
+    }
+
+    let tree = []
+
+    for(let index=0; index<headings.length;index++){
+        let element = headings[index]
+        let parent = find_parent(index,headings)
+        if(parent){
+            parent.items.push(element)
+        }else{
+            tree.push(element)
+        }
+    }
+
+    for(let element of headings){
+        if (element.items.length == 0){
+            element.parent = false
+            delete element.items
+            delete element.expanded
+        }
+    }
+    return tree
+}
+
+/** headings start at Sidemenu
+ * 
+ */
+function process_toc_list(headings){
+    if(typeof headings == 'undefined' || headings.length == 0){
+        return {items:[],visible:false}
+    }
+
+    let side_menu = {items:[],visible:false}
+    side_menu.items = heading_list_to_tree(headings)//also .slug=>.href
+    side_menu.visible = true
+    return side_menu
 }
 
 export{

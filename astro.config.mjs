4a90e92c35339ad90e935524f988a784a24b9dd1
wass <wassim.filali@gmail.com>
2022-10-17 20:03:57 +0200

    changed to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
-import remarkPlantUML from '@akebifiky/remark-simple-plantuml'
+import remarkPlantUML from '@akebifiky/remark-simple-plantuml';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: 'shiki',
-    remarkPlugins:[
-      [remarkPlantUML,{ baseUrl: "https://www.plantuml.com/plantuml/svg" }]
-    ]
+    remarkPlugins: [[remarkPlantUML, {
+      baseUrl: "https://www.plantuml.com/plantuml/svg"
+    }]]
   },
   integrations: [mdx()]
 });

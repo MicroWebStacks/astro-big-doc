1d0aba4bf8638a9bdbdd7e68cf80bd40b199b278
wass <wassim.filali@gmail.com>
2022-10-17 19:53:51 +0200

    fallback to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: 'shiki',
     remarkPlugins:[
-      remarkPlantUML
+      [remarkPlantUML,{ baseUrl: "https://www.plantuml.com/plantuml/svg" }]
     ]
   },
   integrations: [mdx()]

7e3910f30bcb0630e74ffdbc0b31a100cedeedd0
wass <wassim.filali@gmail.com>
2022-10-23 17:22:55 +0200

    revert node import

--- astro.server.config.mjs
+++ astro.server.config.mjs
@@ -1,8 +1,10 @@
+import node from '@astrojs/node'
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import {remarkPUMLObj} from './src/libs/remark-plantuml-object'
 import {remarkPUMLSvg} from './src/libs/remark-plantuml-svg'
 import {remarkPUMLAstro} from './src/libs/remark-plantuml-astro'
+
 
 // https://astro.build/config
 export default defineConfig({

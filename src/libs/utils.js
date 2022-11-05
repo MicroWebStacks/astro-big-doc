01c3a7d34834cc17ebc0b2b97da01134b5f23c95
wass <wassim.filali@gmail.com>
2022-11-05 13:06:05 +0100

    expanded param to gallery started remark refactor un function for rel assets

---
+++ src/libs/utils.js
@@ -0,0 +1,29 @@
+import config from '../../astro.config.mjs'
+
+function relAssetToUrl(relativepath,refdir){
+    let newurl = relativepath
+    const filepath = join(refdir,relativepath)
+    if(existsSync(filepath)){
+      const rootdir = join(dirname(normalize(import.meta.url)),"../..").replace("file:\\","")
+      const targetroot = join(rootdir,"public/raw")
+      const filerootrel = relative(rootdir,refdir)
+      const targetpath = resolve(targetroot,filerootrel)
+      const targetfile = join(targetpath,relativepath)
+      const targetdir = dirname(targetfile)
+      //console.log(`copy from '${filepath}' to '${targetfile}'`)
+      const newpath = join("raw/",filerootrel,relativepath)
+      newurl = "/"+config.base +"/"+ newpath.replaceAll('\\','/')
+      //console.log(`  * new asset url = '${newurl}'`)
+      if(!existsSync(targetdir)){
+        mkdirSync(targetdir,{ recursive: true })
+      }
+      copyFileSync(filepath,targetfile)
+    }
+
+    return newurl
+}
+
+export{
+    relAssetToUrl
+}
+
--- src/pages/images.md
+++ src/pages/images.md
@@ -1,7 +1,21 @@
 ---
 layout: "@/layout/Layout.astro"
-title: "Code Highlight"
+title: "Relative images"
 ---
 # Image in Markdown
+this is a relative image embed. Thanks to the `remark-rel-asset` plugin, it works in Astro same as in vscode and in github.
+
+```markdown
+![github dark](./github-dark.png)
+```
+
 ![github dark](./github-dark.png)
 
+this is a relative svg embed
+
+
+```markdown
+![Tree](./tree.svg)
+```
+
+![Tree](./tree.svg)

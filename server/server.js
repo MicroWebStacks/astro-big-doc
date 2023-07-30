ec46b951b3e8c73a871b6cf300b6ca60409b2ad1
wass <wassim.filali@gmail.com>
2023-07-30 10:16:14 +0200

    fix rel files path

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import https from 'https'
 import { handler as ssrHandler } from '../dist/server/entry.mjs';
 import {authRouter} from './auth/auth_router.js'
-import {rel_to_abs} from '../src/libs/utils.js'
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
 import { readFileSync, } from 'fs';
 
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.static('dist/client/'))
@@ -22,8 +25,8 @@ app.use((req, res, next) => {
 const use_https = ["true","1"].includes(process.env.USE_HTTPS.toLowerCase())
 
 if(use_https){
-    const key = readFileSync(rel_to_abs(import.meta.url,process.env.KEY_FILE),'utf8')
-    const cert = readFileSync(rel_to_abs(import.meta.url,process.env.CERT_FILE),'utf8')
+    const key = readFileSync(join(__dirname, process.env.KEY_FILE),'utf8')
+    const cert = readFileSync(join(__dirname, process.env.CERT_FILE),'utf8')
     const httpsServer = https.createServer({key,cert},app)
     console.log(`listening on ${process.env.HOST}:${process.env.PORT}`)
     httpsServer.listen(3000);//443 for clear url

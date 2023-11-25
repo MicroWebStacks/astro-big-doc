529cb259d345a3ace013bdb0aa549cc88859173f
wass <wassim.filali@gmail.com>
2023-11-25 11:51:09 +0100

    restructured files

--- src/layout/layout_utils.js
+++ src/layout/layout_utils.js
@@ -13,20 +13,6 @@ function section_from_pathname(pathname){
     }
 }
 
-function section_from_url(url){
-    section_from_pathname(new URL(url).pathname)
-}
-
-function get_active_menu(raw_menu,pathname){
-    const current_section = section_from_pathname(pathname)
-    console.log(`current_section = '${current_section}'`)
-    return raw_menu.map((item)=>{
-        item.active_class = (section_from_pathname(item.href) == current_section)?"active":""
-        return item
-    })
-}
-
 export{
-    get_active_menu,
     section_from_pathname
 }

f71c3d45e6c0b0e73ab91c69e50603d5d3d1244d
wass <wassim.filali@gmail.com>
2024-04-13 18:14:23 +0200

    set default server config

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import https from 'https'
-import {authRouter} from './auth/auth_router.js'
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
 import { readFileSync, } from 'fs';
@@ -11,8 +10,10 @@ dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const outdir = (process.env.OUT_DIR==null)?"dist":process.env.OUT_DIR
+const protocol = (process.env.PROTOCOL==null)?"http":process.env.PROTOCOL
+const host = (process.env.HOST==null)?"127.0.0.1":process.env.HOST
+const port = (process.env.PORT==null)?"3001":process.env.PORT
 
 const app = express();
 if(process.env.ENABLE_CORS == "true"){
@@ -20,7 +21,8 @@ if(process.env.ENABLE_CORS == "true"){
     console.log("\n -- !!! CORS enabled !!! -- APIs can be used from other sites --\n")
 }
 
-if(process.env.DISABLE_AUTH != "true"){
+if(process.env.ENABLE_AUTH === "true"){
+    const { authRouter } = await import('./auth/auth_router.js');
     app.use(authRouter)
     console.log(" with auth")
 }else{
@@ -33,13 +35,14 @@ app.use((req, res, next) => {
   })
 
 
-console.log(`listening on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`)
 
-if(process.env.PROTOCOL == "https"){
+console.log(`listening on ${protocol}://${host}:${port}`)
+
+if(protocol == "https"){
     const key = readFileSync(join(__dirname, process.env.KEY_FILE),'utf8')
     const cert = readFileSync(join(__dirname, process.env.CERT_FILE),'utf8')
     const httpsServer = https.createServer({key,cert},app)
-    httpsServer.listen(process.env.PORT);//443 for clear url
+    httpsServer.listen(port);//443 for clear url
 }else{
-    app.listen(process.env.PORT);
+    app.listen(port);
 }
